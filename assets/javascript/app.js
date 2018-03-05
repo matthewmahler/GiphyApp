5457d05be77bb6a6616a0217b245895d8b1924e0
matthewmahler <matthewjamesmahler@gmail.com>
2018-03-05 12:42:59 -0500

    initial format and style

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+// l o g i c
+
+//on page load to the api call to go get the trending catagories
+//put them in an array
+//loop through that array to create buttons for each catagory
+// display those buttons on the div on the left
+
+//on search go to the api 
+// associate its reponse with a new button
+// call the display function again
+
+//edge cases
+//when you click the saem button twice dont replace the gifs on screen
+// if displaying a different catagory, clear the div firsts and display the new stuff
+
+
+//functions
+//api call
+//display
+//search
+//append the left div
+//diplay the gifs
+//empty the gif window
+
+
+
+// variables
+//gifs
+//catagories
+//
+
+
+
+
+
+// on clicks
+//search btn
+//each of the created btns
