6d0e9148bdef9cf7e8803eb2966e6f7de2ee1b1e
matthewmahler <matthewjamesmahler@gmail.com>
2018-03-06 19:28:25 -0500

    just messed with the layout more

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,55 @@
+var gif = "";
+var queryURL = ""
+var recentSearches = []
+
+$( document ).ready(function() {
+  console.log(gif)
+  var $searchButton = document.getElementById('search-button');
+  $searchButton.addEventListener('click', searchForGifs);
+
+  function searchForGifs(event){
+    event.preventDefault();
+    var gif = $("#gif-input").val().trim();
+    console.log(gif)
+    recentSearches.push(gif);
+    console.log(recentSearches);
+    if (gif != ""){
+    renderButtons();
+  }
+  }
+
+function renderButtons() {
+
+        $("#search-btns").empty();
+
+        for (var i = 0; i < recentSearches.length; i++) {
+
+          var a = $("<button>");
+          a.addClass("new-btn");
+          a.attr("data-name", recentSearches[i]);
+          a.text(recentSearches[i]);
+          $("#search-btns").append(a);
+          $("#gif-input").val('')
+        }
+      }
+
+
+$.ajax({
+  url: "http://api.giphy.com/v1/gifs/search?q=" + queryURL + "&api_key=6TAb9rarJAjNgMlX5O5nfBpjC2ZZXEhY&limit=10",
+  method: "GET"
+}).then(function(response) {
+
+console.log(response)
+
+;})
+});
+
+
+
+//my key
+//6TAb9rarJAjNgMlX5O5nfBpjC2ZZXEhY
+
+
 // l o g i c
 
 //on page load to the api call to go get the trending catagories
@@ -28,10 +80,6 @@
 //gifs
 //catagories
 //
-
-
-
-
 
 // on clicks
 //search btn
